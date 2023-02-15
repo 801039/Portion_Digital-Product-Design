22072a8264de403b9e9c9a7400edd7520be99ba6
Tharindu Sanjitha <65927967+801039@users.noreply.github.com>
2023-02-15 11:04:39 +0530

    Siple Table

---
+++ src/components/columns.jsx
@@ -0,0 +1,24 @@
+
+
+export const COLUMNS = [
+    {
+        Header:'⠇ID',
+        accessor:'ID'
+    },
+    {
+        Header:'NAME',
+        accessor:'NAME'
+    },
+    {
+        Header:'PHONE',
+        accessor:'PHONE'
+    },
+    {
+        Header:'EMAIL',
+        accessor:'EMAIL'
+    },
+    {
+        Header:'DATE OF JOINING',
+        accessor:'DATE OF JOINING'
+    },  
+]
